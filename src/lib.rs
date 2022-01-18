76f3a44285fae18fb44a671f3b2bd253bc1884ca
Ildar Mulyukov <ildar@altlinux.ru>
2022-01-18 14:05:58 +0600

    add LoopHandle

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ impl EventLoop<'_> {
         let el = EventLoop(rs_calloop::EventLoop::try_new()?);
         res.set("_self", lua.create_userdata(el)?)?;
         res.set("get_signal", lua.create_function(EventLoop::get_signal)?)?;
+        res.set("handle", lua.create_function(EventLoop::handle)?)?;
         res.set("run", lua.create_function(EventLoop::run)?)?;
         Ok(res)
     }
@@ -38,6 +39,16 @@ impl EventLoop<'_> {
                 .borrow::<EventLoop>()?
                 .0
                 .get_signal()
+            )?;
+        Ok(res)
+    }
+
+    fn handle<'a>(lua: &'static Lua, luaself: LuaTable<'a>) -> LuaResult<LuaTable<'a>> {
+        let res = LoopHandle::new(lua,
+                luaself.get::<_, LuaAnyUserData>("_self")?
+                .borrow::<EventLoop>()?
+                .0
+                .handle()
             )?;
         Ok(res)
     }
@@ -84,6 +95,36 @@ impl LoopSignal {
         let ud: LuaAnyUserData = luaself.get("_self")?;
         let ref_self = ud.borrow::<Self>()?;
         ref_self.0.wakeup();
+        Ok(())
+    }
+}
+
+// LoopHandle
+#[derive(Clone)]
+struct LoopHandle<'lh> (rs_calloop::LoopHandle<'lh, LuaTable<'lh>>);
+impl LuaUserData for LoopHandle<'_> {}
+impl LoopHandle<'_> {
+    fn new<'lh>(lua: &'static Lua, lh: rs_calloop::LoopHandle<'static, LuaTable<'static>>) -> LuaResult<LuaTable<'lh>> {
+        let res = lua.create_table()?;
+        res.set("_self",
+            lua.create_userdata(
+                LoopHandle(lh)
+            )?)?;
+        res.set("insert_idle", lua.create_function(LoopHandle::insert_idle)?)?;
+        Ok(res)
+    }
+
+    pub fn insert_idle(_: &Lua, (luaself, callback): (LuaTable, Option<LuaFunction<'static>>)) -> LuaResult<()> {
+        let ud: LuaAnyUserData = luaself.get("_self")?;
+        let ref_self = ud.borrow::<LoopHandle<'static>>()?;
+        ref_self.0.insert_idle(
+            move |data| {
+                if callback.is_some() {
+                    let cb = callback.as_ref();
+                    cb.unwrap().call::<LuaTable,()>( (*data).clone() )
+                        .unwrap();
+                }
+            });
         Ok(())
     }
 }
